ba20c8698bfcc33b77470382f4678631b43b3579
Henrik Fröhling <henrik.froehling@gmail.com>
2020-04-26 01:48:32 +0200

    Add first delphi test files

---
+++ testfiles/Delphi/SimpleProgram.pas
@@ -0,0 +1,11 @@
+program Greeting;
+
+{$APPTYPE CONSOLE}
+
+var
+  MyMessage: string;
+
+begin
+  MyMessage := 'Hello world!';
+  Writeln(MyMessage);
+end.
---
+++ testfiles/Delphi/SmallUnit.pas
@@ -0,0 +1,14 @@
+unit Unit1;
+
+interface
+
+procedure PrintMessage(msg: string);
+
+implementation
+
+procedure PrintMessage(msg: string);
+begin
+  Writeln(msg);
+end;
+
+end.
