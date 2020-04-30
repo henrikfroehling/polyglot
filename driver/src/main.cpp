c136da3005683bc19bd696c6072ca6d76b947992
Henrik Fröhling <henrik.froehling@gmail.com>
2020-04-30 16:11:32 +0200

    Reading input file in library driver

--- driver/src/main.cpp
+++ driver/src/main.cpp
@@ -1,17 +1,33 @@
+#include <fstream>
+#include <streambuf>
 #include <string>
 #include <polyglot/CodeAnalysis/CodeAnalysis.hpp>
 #include <polyglot/CodeAnalysis/Core/Syntax/SyntaxTree.hpp>
 
-int main()
+std::string readFile(const std::string& filename);
+
+int main(int argc,
+         char** argv)
 {
     using namespace polyglot::CodeAnalysis;
 
-    const std::string sourceText{"unit Unit1;\n\n"
-                                 "interface\n\n"
-                                 "implementation\n\n"
-                                 "end.\n"};
-
+    const std::string sourceText = readFile(argv[1]);
     SyntaxTree* syntaxTree = Delphi::parseSourceText(sourceText);
     delete syntaxTree;
     return 0;
+}
+
+std::string readFile(const std::string& filename)
+{
+    std::ifstream file{filename};
+    std::string str;
+
+    file.seekg(0, std::ios::end);
+    str.reserve(file.tellg());
+    file.seekg(0, std::ios::beg);
+
+    str.assign((std::istreambuf_iterator<char>(file)),
+                std::istreambuf_iterator<char>());
+
+    return str;
 }
