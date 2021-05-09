72990ded3569cc490531a70c3c830a19c4bb2749
Henrik Fröhling <henrik.froehling@gmail.com>
2021-05-09 14:22:54 +0200

    Expose public interface types

--- lib/include/polyglot/Polyglot.hpp
+++ lib/include/polyglot/Polyglot.hpp
@@ -1,4 +1,21 @@
 #ifndef POLYGLOT_POLYGLOT_H
 #define POLYGLOT_POLYGLOT_H
 
+#include "polyglot/Core/Syntax/ISyntaxNode.hpp"
+#include "polyglot/Core/Syntax/ISyntaxToken.hpp"
+#include "polyglot/Core/Syntax/ISyntaxTree.hpp"
+#include "polyglot/Core/Syntax/ISyntaxTrivia.hpp"
+#include "polyglot/Core/Syntax/ISyntaxTriviaList.hpp"
+#include "polyglot/Core/Syntax/SyntaxKinds.hpp"
+#include "polyglot/Core/Syntax/SyntaxNodeOrToken.hpp"
+
+#include "polyglot/Core/Text/TextSpan.hpp"
+
+#include "polyglot/Core/LanguageKind.hpp"
+#include "polyglot/Core/Types.hpp"
+
+#include "polyglot/Version.hpp"
+
+#include "polyglot/polyglot_global.hpp"
+
 #endif // POLYGLOT_POLYGLOT_H
