54d586531d28a6d4023290c01faf6ba160101245
Henrik Fröhling <henrik.froehling@gmail.com>
2021-05-22 20:26:12 +0200

    Fix errors

--- driver/src/main.cpp
+++ driver/src/main.cpp
@@ -1,15 +1,15 @@
 #include <fstream>
 #include <iostream>
 #include <streambuf>
-#include <string>
+#include <polyglot/Core/Types.hpp>
 #include <polyglot/Delphi/Syntax/IDelphiSyntaxTree.hpp>
 
-std::string readFile(const std::string& filename);
+pg_string readFile(const pg_string& filename);
 
 int main(int argc,
-         char** argv)
+         pg_char** argv)
 {
-    const std::string sourceText = readFile(argv[1]);
+    const pg_string sourceText = readFile(argv[1]);
     auto ptrSyntaxTree = polyglot::Delphi::Syntax::IDelphiSyntaxTree::parseSourceText(argv[1], sourceText);
 
     std::string str;
@@ -18,17 +18,17 @@ int main(int argc,
     return 0;
 }
 
-std::string readFile(const std::string& filename)
+pg_string readFile(const pg_string& filename)
 {
-    std::ifstream file{filename};
-    std::string str;
+    std::wifstream file{filename};
+    pg_string str;
 
     file.seekg(0, std::ios::end);
     str.reserve(file.tellg());
     file.seekg(0, std::ios::beg);
 
-    str.assign((std::istreambuf_iterator<char>(file)),
-                std::istreambuf_iterator<char>());
+    str.assign((std::istreambuf_iterator<pg_char>(file)),
+                std::istreambuf_iterator<pg_char>());
 
     return str;
 }
